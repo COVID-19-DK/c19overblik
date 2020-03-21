74672bb1100c348268a37b9ea6b88294b94c2eec
emillykkejensen <emil@emillykkejensen.dk>
2020-03-21 13:49:11 +0000

    Forbedrede UI og tilføjet landeliste som man kan vælge fra

--- server.R
+++ server.R
@@ -1,47 +1,51 @@
 function(input, output) {
     
-    GlobalTimeline <- reactive({
-        get_GlobalTimeline()
-    })
-    
+    GlobalTimeline <- get_GlobalTimeline()
 
     output$plot_GlobalTimeline <- highcharter::renderHighchart({
         
         show_x <- input$show_x
         show_y <- input$show_y
+        country <- input$country
         
-        shiny::withProgress(message = "Henter data...", value = NULL, {
-            
-            GlobalTimeline <- GlobalTimeline()
-            
-            if(show_x == "date") GlobalTimeline[, x := date]
-            if(show_x == "infected"){
-                GlobalTimeline <- GlobalTimeline[!is.na(totalcases)]
-                setorder(GlobalTimeline, countrylabel, date)
-                GlobalTimeline[, x := rowid(countrylabel)]
-            }
-            if(show_x == "dead"){
-                GlobalTimeline <- GlobalTimeline[!is.na(totaldeaths)]
-                setorder(GlobalTimeline, countrylabel, date)
-                GlobalTimeline[, x := rowid(countrylabel)]
-            }
-            
-            if(show_y == "infected") GlobalTimeline[, y := totalcases]
-            if(show_y == "dead") GlobalTimeline[, y := totaldeaths]
-            if(show_y == "recovered") GlobalTimeline[, y := totalrecovered]
-            
-            highchart() %>% 
-                hc_add_theme(hc_theme_elementary()) %>%
-                hc_tooltip(headerFormat = "",
-                           pointFormat = "<b>{series.name}</b><br/>Antal registrede smittede: {point.infected}<br/>Antal døde: {point.dead}<br/>Antal raske: {point.recovered}<br/>") %>%
-                hc_add_series(data = GlobalTimeline,
-                              type = "line",
-                              mapping = hcaes(x = x, y = y, group = countrylabel, infected = totalcases, dead = totaldeaths, recovered = totalrecovered))
-            
-            
-        })
+        GlobalTimeline <- GlobalTimeline[countrylabel %in% country]
         
+        if(show_x == "date") GlobalTimeline[, x := date]
+        if(show_x == "infected"){
+            GlobalTimeline <- GlobalTimeline[!is.na(totalcases)]
+            setorder(GlobalTimeline, countrylabel, date)
+            GlobalTimeline[, x := rowid(countrylabel)]
+        }
+        if(show_x == "dead"){
+            GlobalTimeline <- GlobalTimeline[!is.na(totaldeaths)]
+            setorder(GlobalTimeline, countrylabel, date)
+            GlobalTimeline[, x := rowid(countrylabel)]
+        }
         
+        if(show_y == "infected") GlobalTimeline[, y := totalcases]
+        if(show_y == "dead") GlobalTimeline[, y := totaldeaths]
+        if(show_y == "recovered") GlobalTimeline[, y := totalrecovered]
+        
+        x_tootltip <- switch(show_x, date = "", infected = "{point.x} dage siden første registerede smittede<br/>", dead = "{point.x} dage siden første dødsfald<br/>")
+        
+        hc_plot <- 
+            highchart() %>%
+            hc_add_theme(hc_theme_elementary()) %>%
+            hc_tooltip(headerFormat = "",
+                       pointFormat = paste0(
+                           "<b>{series.name}</b><br/>
+                               Dato: {point.dato}<br/>
+                               ", x_tootltip, "
+                               Antal registrede smittede: {point.infected}<br/>
+                               Antal døde: {point.dead}<br/>
+                               Antal raske: {point.recovered}<br/>")) %>%
+            hc_add_series(data = GlobalTimeline,
+                          type = "line",
+                          mapping = hcaes(x = x, y = y, group = countrylabel, dato = format(date, "%d-%m-%y"), infected = totalcases, dead = totaldeaths, recovered = totalrecovered))
+        
+        if(show_x == "date") hc_plot <- hc_xAxis(hc_plot, type = "datetime")
+        
+        hc_plot
         
     })
 }
--- ui.R
+++ ui.R
@@ -1,35 +1,78 @@
 fluidPage(
   
-  # Application title
   titlePanel("C19 Overblik"),
   
-  mainPanel(width = 12,
+  mainPanel(
+    width = 12,
     
-    highcharter::highchartOutput("plot_GlobalTimeline"),
-    
-    radioGroupButtons(
-      inputId = "show_x",
-      label = "Vis tid efter:", 
-      choices = c("Dato" = "date", "Dage fra første registerede smittede" = "infected", "Dage fra første registede død" = "dead"), 
-      justified = FALSE,
-      size = "sm",
-      selected = "date",
-      status = "primary",
-      checkIcon = list(yes = icon("ok", lib = "glyphicon"), no = icon("remove", lib = "glyphicon"))
-    ),
-    
-    radioGroupButtons(
-      inputId = "show_y",
-      label = "Vis:", 
-      choices = c("Antal registrede smittede" = "infected", "Antal døde" = "dead", "Antal raske" = "recovered"), 
-      justified = FALSE,
-      size = "sm",
-      selected = "infected",
-      status = "primary",
-      checkIcon = list(yes = icon("ok", lib = "glyphicon"), no = icon("remove", lib = "glyphicon"))
+    tabsetPanel(
+      type = "tabs",
+      
+      # tabPanel("Danmark"),
+      
+      tabPanel(
+        title = "Global",
+        
+        div(
+          style="margin-top: 10px; margin-left: 10px;",
+          
+          div(
+            style = "float: left;",
+            pickerInput(
+              inputId = "country",
+              label = "Lande:", 
+              choices = landelist,
+              selected = c("Denmark", "Finland", "Norway", "Sweden", "USA", "UK", "Italy", "Germany", "France", "China"),
+              multiple = TRUE,
+              options = list(
+                `live-search` = TRUE,
+                `actions-box` = TRUE)
+            )
+          ),
+          
+          div(
+            style = "float: left; margin-left: 20px;",
+            radioGroupButtons(
+              inputId = "show_x",
+              label = "Vis tid efter:", 
+              choices = c("Dato" = "date", "Dage fra første registerede smittede" = "infected", "Dage fra første registede død" = "dead"), 
+              justified = FALSE,
+              size = "sm",
+              selected = "date",
+              status = "primary",
+              checkIcon = list(
+                yes = tags$i(class = "fa fa-circle", 
+                             style = "color: steelblue"),
+                no = tags$i(class = "fa fa-circle-o", 
+                            style = "color: steelblue"))
+            )
+          ),
+          
+          div(
+            style = "float: left; margin-left: 20px;",
+            radioGroupButtons(
+              inputId = "show_y",
+              label = "Vis:", 
+              choices = c("Antal registrede smittede" = "infected", "Antal døde" = "dead", "Antal raske" = "recovered"), 
+              justified = FALSE,
+              size = "sm",
+              selected = "infected",
+              status = "primary",
+              checkIcon = list(
+                yes = tags$i(class = "fa fa-circle", 
+                             style = "color: steelblue"),
+                no = tags$i(class = "fa fa-circle-o", 
+                            style = "color: steelblue")  
+              )
+            )
+          )
+          
+        ),
+        
+        div(style = "clear: left", highchartOutput("plot_GlobalTimeline", height = "calc(100vh - 220px)")),
+        
+      )
+      
     )
-    
-    
   )
-  
 )
